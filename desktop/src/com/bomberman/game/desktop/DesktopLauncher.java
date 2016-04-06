ca9eb8a3169e3757955cfab6877c84c44455e094
huddy14 <huddy14@gmail.com>
2016-04-06 14:42:37 +0200

    first

---
+++ desktop/src/com/bomberman/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bomberman.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bomberman.game.Bomberman;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bomberman(), config);
+	}
+}
